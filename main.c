24680373255c463b97f55cbdd75d140e5b261c58
Koru-zed <mait-jao@student.1337.ma>
2022-10-12 11:43:52 +0100

    parsing almost done : adding rt_last_list to Plane , Sphere, Cylinder

--- main.c
+++ main.c
@@ -95,69 +95,79 @@ void    set_cordinates(char const *cord, float *table, t_minirt *mini)
     }
 }
 
-t_Ambient *ft_alloc_Ambient()
-{
-    t_Ambient *ambient;
+// t_Ambient *ft_alloc_Ambient()
+// {
+//     t_Ambient *_ambient;
 
-    ambient = ft_calloc(1, sizeof(t_Ambient));
-    // ambient->color = ft_calloc(3, sizeof(size_t));
-    ft_bzero(ambient->color, 3);
-    return (ambient);
-}
+//     _ambient = ft_calloc(1, sizeof(t_Ambient));
+//     // _ambient->color = ft_calloc(3, sizeof(size_t));
+//     // ft_bzero(_ambient->color, 3);
+//     return (_ambient);
+// }
 
-t_Camera *ft_alloc_Camera()
-{
-    t_Camera *camera;
+// t_Camera *ft_alloc_Camera()
+// {
+//     t_Camera *_camera;
 
-    camera = ft_calloc(1, sizeof(t_Camera));
-    // camera->cordinates = ft_calloc(3, sizeof(float));
-    // camera->orientation = ft_calloc(3, sizeof(float));
-    ft_bzero(camera->cordinates, 3);
-    ft_bzero(camera->orientation, 3);
-    return (camera);
-}
+//     _camera = ft_calloc(1, sizeof(t_Camera));
+//     // _camera->cordinates = ft_calloc(3, sizeof(float));
+//     // _camera->orientation = ft_calloc(3, sizeof(float));
+//     // ft_bzero(_camera->cordinates, 3);
+//     // ft_bzero(_camera->orientation, 3);
+//     return (_camera);
+// }
 
-t_Light *ft_alloc_Light()
-{
-    t_Light *light;
+// t_Light *ft_alloc_Light()
+// {
+//     t_Light *_light;
 
-    light = ft_calloc(1, sizeof(t_Light));
-    ft_bzero(light->cordinates, 3);
-    ft_bzero(light->color, 3);
-    return (light);
-}
+//     _light = ft_calloc(1, sizeof(t_Light));
+//     // _light->cordinates = ft_calloc(3, sizeof(float));
+//     // _light->color = ft_calloc(3, sizeof(size_t));
+//     // ft_bzero(_light->cordinates, 3);
+//     // ft_bzero(_light->color, 3);
+//     return (_light);
+// }
 
-t_Plane *ft_alloc_Light()
-{
-    t_Plane *plane;
+// t_Plane *ft_alloc_Plane()
+// {
+//     t_Plane *_plane;
 
-    plane = ft_calloc(1, sizeof(t_Plane));
-    ft_bzero(plane->cordinates, 3);
-    ft_bzero(plane->orientation, 3);
-    ft_bzero(plane->color, 3);
-    return (plane);
-}
+//     _plane = ft_calloc(1, sizeof(t_Plane));
+//     // _plane->cordinates = ft_calloc(3, sizeof(float));
+//     // _plane->orientation = ft_calloc(3, sizeof(float));
+//     // _plane->color = ft_calloc(3, sizeof(size_t));
+//     // ft_bzero(_plane->cordinates, 3);
+//     // ft_bzero(_plane->orientation, 3);
+//     // ft_bzero(_plane->color, 3);
+//     return (_plane);
+// }
 
-t_Sphere *ft_alloc_Light()
-{
-    t_Sphere *sphere;
+// t_Sphere *ft_alloc_Sphere()
+// {
+//     t_Sphere *_sphere;
 
-    sphere = ft_calloc(1, sizeof(t_Sphere));
-    ft_bzero(sphere->cordinates, 3);
-    ft_bzero(sphere->color, 3);
-    return (sphere);
-}
+//     _sphere = ft_calloc(1, sizeof(t_Sphere));
+//     // ft_bzero(_sphere->cordinates, 3);
+//     // ft_bzero(_sphere->color, 3);
+//     // _sphere->cordinates = ft_calloc(3, sizeof(float));
+//     // _sphere->color = ft_calloc(3, sizeof(size_t));
+//     return (_sphere);
+// }
 
-t_Cylinder *ft_alloc_Light()
-{
-    t_Cylinder *cylinder;
+// t_Cylinder *ft_alloc_Cylinder()
+// {
+//     t_Cylinder *_cylinder;
 
-    cylinder = ft_calloc(1, sizeof(t_Cylinder));
-    ft_bzero(cylinder->cordinates, 3);
-    ft_bzero(cylinder->orientation, 3);
-    ft_bzero(cylinder->color, 3);
-    return (cylinder);
-}
+//     _cylinder = ft_calloc(1, sizeof(t_Cylinder));
+//     // ft_bzero(_cylinder->cordinates, 3);
+//     // ft_bzero(_cylinder->orientation, 3);
+//     // ft_bzero(_cylinder->color, 3);
+//     // _cylinder->cordinates = ft_calloc(3, sizeof(float));
+//     // _cylinder->orientation = ft_calloc(3, sizeof(float));
+//     // _cylinder->color = ft_calloc(3, sizeof(size_t));
+//     return (_cylinder);
+// }
 
 void    set_orientation(char const *colors, float *table, t_minirt *mini)
 {
@@ -286,6 +296,43 @@ int    get_Light(t_minirt *mini, t_data *data)
 
 }
 
+t_Plane	*rt_last_Plane(t_Plane *Plane)
+{
+	t_Plane     *p_lst;
+	p_lst = Plane;
+	if (p_lst)
+	{
+		while (p_lst->next)
+			p_lst = p_lst->next;
+	}
+	return (p_lst);
+}
+
+t_Sphere	*rt_last_Sphere(t_Sphere *Sphere)
+{
+	t_Sphere     *s_lst;
+
+	s_lst = Sphere;
+	if (s_lst)
+	{
+		while (s_lst->next)
+			s_lst = s_lst->next;
+	}
+	return (s_lst);
+}
+
+t_Cylinder	*rt_last_Cylinder(t_Cylinder *Cylinder)
+{
+	t_Cylinder     *c_lst;
+
+	c_lst = Cylinder;
+	if (c_lst)
+	{
+		while (c_lst->next)
+			c_lst = c_lst->next;
+	}
+	return (c_lst);
+}
 // t_Plane *get_node_Plane(t_Plane *_plane)
 // {
 //     size_t     i;
@@ -311,31 +358,32 @@ int    get_Light(t_minirt *mini, t_data *data)
 
 int    get_Plane(t_minirt *mini, t_data *data)
 {
-    size_t     i;
+    int     empty;
     t_Plane *_plane;
 
-    _plane = mini->Plane;
-    i = mini->Plane->repetition;
-    while (_plane)
-        _plane = _plane->next;
-    _plane = ft_alloc_Plane();
+    empty = 1;
+    if (!mini->Plane)
+        empty = 0;
+    _plane = ft_calloc(1, sizeof(t_Plane));
     set_cordinates(data->pars[1], _plane->cordinates, mini);
     set_orientation(data->pars[2], _plane->orientation, mini);
     set_color(data->pars[3], _plane->color, mini);
-    mini->Plane->repetition++;
+    if (empty)
+        rt_last_Plane(mini->Plane)->next = _plane;
+    else
+        mini->Plane = _plane;
     return 1;
 }
 
 int    get_Sphere(t_minirt *mini, t_data *data)//**Spher
 {
-    size_t     i;
+    int      empty;
     t_Sphere *_sphere;
 
-    _sphere = mini->Sphere;
-    i = mini->Sphere->repetition;
-    while (_sphere)
-        _sphere = _sphere->next;
-    _sphere = ft_alloc_Sphere();
+    empty = 1;
+    if (!mini->Sphere)
+        empty = 0;
+    _sphere = ft_calloc(1, sizeof(t_Sphere));
     set_cordinates(data->pars[1], _sphere->cordinates, mini);
     _sphere->diameter = ft_atod(data->pars[2], &mini->check);
     if (mini->check)
@@ -344,19 +392,22 @@ int    get_Sphere(t_minirt *mini, t_data *data)//**Spher
         exit(EXIT_FAILURE);
     }
     set_color(data->pars[3], _sphere->color, mini);
+    if (empty)
+        rt_last_Sphere(mini->Sphere)->next = _sphere;
+    else
+        mini->Sphere = _sphere;
     return 1;
 }
 
 int    get_Cylinder(t_minirt *mini, t_data *data)
 {
-    size_t     i;
+    int         empty;
     t_Cylinder *_cylinder;
 
-    _cylinder = mini->Cylinder;
-    i = mini->Cylinder->repetition;
-    while (_cylinder)
-        _cylinder = _cylinder->next;
-    _cylinder = ft_alloc_Cylinder();
+    empty = 1;
+    if (!mini->Cylinder)
+        empty = 0;
+    _cylinder = ft_calloc(1, sizeof(t_Cylinder));
     set_cordinates(data->pars[1], _cylinder->cordinates, mini);
     set_orientation(data->pars[2], _cylinder->orientation, mini);
     _cylinder->diameter = ft_atod(data->pars[3], &mini->check);
@@ -367,6 +418,10 @@ int    get_Cylinder(t_minirt *mini, t_data *data)
         exit(EXIT_FAILURE);
     }
     set_color(data->pars[5], _cylinder->color, mini);
+    if (empty)
+        rt_last_Cylinder(mini->Cylinder)->next = _cylinder;
+    else
+        mini->Cylinder = _cylinder;
     return 1;
 }
 
@@ -386,6 +441,7 @@ size_t    check_data(t_minirt *mini, t_data *data)
     // {
         if (!ft_strcmp(data->pars[0], ambient) && get_Ambient_lightning(mini, data))
             {
+                printf("-- Ambient --\n");
                 printf("************************\n");
                 printf("repetition [%zu]\n", mini->Ambient->repetition);
                 printf("ratio [%f]\n", mini->Ambient->ratio);
@@ -397,6 +453,7 @@ size_t    check_data(t_minirt *mini, t_data *data)
 ////////////////////////////////////////////////////////////////////////////////////////////////
         else if (!ft_strcmp(data->pars[0], camera) && get_Camera(mini, data)) 
             {
+                printf("-- Camera --\n");
                 printf("************************\n");
                 printf("repetition [%zu]\n", mini->Camera->repetition);
                 printf("FOV [%zu]\n", mini->Camera->fov);
@@ -412,6 +469,7 @@ size_t    check_data(t_minirt *mini, t_data *data)
 ////////////////////////////////////////////////////////////////////////////////////////////////
         else if (!ft_strcmp(data->pars[0], light) && get_Light(mini, data))            
             {
+                printf("-- Light --\n");
                 printf("************************\n");
                 printf("repetition [%zu]\n", mini->Light->repetition);
                 printf("brightenss [%f]\n", mini->Light->brightenss);
@@ -427,51 +485,55 @@ size_t    check_data(t_minirt *mini, t_data *data)
 ////////////////////////////////////////////////////////////////////////////////////////////////
         else if (!ft_strcmp(data->pars[0], sphre) && get_Sphere(mini, data)) 
             {
+                printf("-- Sphere --\n");
                 printf("************************\n");
-                printf("diameter [%f]\n", mini->Sphere->diameter);
-                printf("X [%f]\n", mini->Sphere->cordinates[0]);
-                printf("Y [%f]\n", mini->Sphere->cordinates[1]);
-                printf("Z [%f]\n", mini->Sphere->cordinates[2]);
+                printf("diameter [%f]\n", rt_last_Sphere(mini->Sphere)->diameter);
+                printf("X [%f]\n", rt_last_Sphere(mini->Sphere)->cordinates[0]);
+                printf("Y [%f]\n", rt_last_Sphere(mini->Sphere)->cordinates[1]);
+                printf("Z [%f]\n", rt_last_Sphere(mini->Sphere)->cordinates[2]);
                     printf("   -----  \n");
-                printf("R [%zu]\n", mini->Sphere->color[0]);
-                printf("G [%zu]\n", mini->Sphere->color[1]);
-                printf("B [%zu]\n", mini->Sphere->color[2]);
+                printf("R [%zu]\n", rt_last_Sphere(mini->Sphere)->color[0]);
+                printf("G [%zu]\n", rt_last_Sphere(mini->Sphere)->color[1]);
+                printf("B [%zu]\n", rt_last_Sphere(mini->Sphere)->color[2]);
                 printf("************************\n");
             }
 ////////////////////////////////////////////////////////////////////////////////////////////////
         else if (!ft_strcmp(data->pars[0], plane) && get_Plane(mini, data)) 
             {
+                printf ("-- Plan --\n");
                 printf("************************\n");
-                printf("X [%f]\n", mini->Plane->cordinates[0]);
-                printf("Y [%f]\n", mini->Plane->cordinates[1]);
-                printf("Z [%f]\n", mini->Plane->cordinates[2]);
+                printf("X [%f]\n", rt_last_Plane(mini->Plane)->cordinates[0]);
+                printf("Y [%f]\n", rt_last_Plane(mini->Plane)->cordinates[1]);
+                printf("Z [%f]\n", rt_last_Plane(mini->Plane)->cordinates[2]);
                     printf("   -----  \n");
-                printf("X.o_v [%f]\n", mini->Plane->orientation[0]);
-                printf("Y.o_v [%f]\n", mini->Plane->orientation[1]);
-                printf("Z.o_v [%f]\n", mini->Plane->orientation[2]);
+                printf("X.o_v [%f]\n", rt_last_Plane(mini->Plane)->orientation[0]);
+                printf("Y.o_v [%f]\n", rt_last_Plane(mini->Plane)->orientation[1]);
+                printf("Z.o_v [%f]\n", rt_last_Plane(mini->Plane)->orientation[2]);
                     printf("   -----  \n");
-                printf("R [%zu]\n", mini->Plane->color[0]);
-                printf("G [%zu]\n", mini->Plane->color[1]);
-                printf("B [%zu]\n", mini->Plane->color[2]);
+                printf("R [%zu]\n", rt_last_Plane(mini->Plane)->color[0]);
+                printf("G [%zu]\n", rt_last_Plane(mini->Plane)->color[1]);
+                printf("B [%zu]\n", rt_last_Plane(mini->Plane)->color[2]);
                 printf("************************\n");
+            // exit (1);
             }
 ////////////////////////////////////////////////////////////////////////////////////////////////
         else if (!ft_strcmp(data->pars[0], cylinder) && get_Cylinder(mini, data)) 
            {
+                printf("-- Cylinder --\n");
                 printf("************************\n");
-                printf("diameter [%f]\n", mini->Cylinder->diameter);
-                printf("hright [%f]\n", mini->Cylinder->hright);
-                printf("X [%f]\n", mini->Cylinder->cordinates[0]);
-                printf("Y [%f]\n", mini->Cylinder->cordinates[1]);
-                printf("Z [%f]\n", mini->Cylinder->cordinates[2]);
+                printf("diameter [%f]\n", rt_last_Cylinder(mini->Cylinder)->diameter);
+                printf("hright [%f]\n", rt_last_Cylinder(mini->Cylinder)->hright);
+                printf("X [%f]\n", rt_last_Cylinder(mini->Cylinder)->cordinates[0]);
+                printf("Y [%f]\n", rt_last_Cylinder(mini->Cylinder)->cordinates[1]);
+                printf("Z [%f]\n", rt_last_Cylinder(mini->Cylinder)->cordinates[2]);
                     printf("   -----  \n");
-                printf("X.o_v [%f]\n", mini->Cylinder->orientation[0]);
-                printf("Y.o_v [%f]\n", mini->Cylinder->orientation[1]);
-                printf("Z.o_v [%f]\n", mini->Cylinder->orientation[2]);
+                printf("X.o_v [%f]\n", rt_last_Cylinder(mini->Cylinder)->orientation[0]);
+                printf("Y.o_v [%f]\n", rt_last_Cylinder(mini->Cylinder)->orientation[1]);
+                printf("Z.o_v [%f]\n", rt_last_Cylinder(mini->Cylinder)->orientation[2]);
                     printf("   -----  \n");
-                printf("R [%zu]\n", mini->Cylinder->color[0]);
-                printf("G [%zu]\n", mini->Cylinder->color[1]);
-                printf("B [%zu]\n", mini->Cylinder->color[2]);
+                printf("R [%zu]\n", rt_last_Cylinder(mini->Cylinder)->color[0]);
+                printf("G [%zu]\n", rt_last_Cylinder(mini->Cylinder)->color[1]);
+                printf("B [%zu]\n", rt_last_Cylinder(mini->Cylinder)->color[2]);
                 printf("************************\n");
             }
     // if (!mini->check)////////////////////////
@@ -508,12 +570,9 @@ int main(int ac, char **av)
         miniRT->file = av[1];
         miniRT->fd = open(miniRT->file, O_RDONLY);
         fill_data(NULL, miniRT->data, miniRT->fd);
-        miniRT->Ambient = ft_alloc_Ambient();
-        miniRT->Camera = ft_alloc_Camera();
-        miniRT->Light = ft_alloc_Light();
-        miniRT->Sphere = ft_alloc_Sphere();
-        miniRT->Plane = ft_alloc_Plane();
-        miniRT->Cylinder = ft_alloc_Cylinder();
+        miniRT->Ambient = ft_calloc(1, sizeof(t_Ambient));
+        miniRT->Camera = ft_calloc(1, sizeof(t_Camera));
+        miniRT->Light = ft_calloc(1, sizeof(t_Light));
         fill_Info(miniRT);
     }
     Data = miniRT->data;
